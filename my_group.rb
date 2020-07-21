501af98cbe2d93bd0f8aac9eb9792000044f7389
munemo <john.chimbani@outlook.com>
2020-07-21 10:04:20 +0200

    first commit

---
+++ my_group.rb
@@ -0,0 +1,9 @@
+my_group = Array.new
+
+person1 = {name: "Thomas", age: 44, weight: "male"}
+person2 = {name: "Sara", age: 39, weight: "female"}
+person3 = {name: "James", age: 22, weight: "male"}
+
+my_group.push(person1)
+my_group.push(person2)
+my_group.push(person3)
